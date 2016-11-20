a48fcacab3f0ed3e1e80e8ea06822f1e5d852ac8
Michael Kilian <michael@kilian24.de>
2016-11-20 22:34:23 +0100

    Add UI Tests

---
+++ MegaAppUITests/MegaAppUITests.swift
@@ -0,0 +1,40 @@
+//
+//  MegaAppUITests.swift
+//  MegaAppUITests
+//
+//  Created by Michael on 20.11.16.
+//  Copyright © 2016 Michael Kilian. All rights reserved.
+//
+
+import XCTest
+
+class MegaAppUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        let app = XCUIApplication()
+        setupSnapshot(app) //Init snapshot
+        app.launch()
+        
+        snapshot("MainScreen") //Take a snapshot
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
